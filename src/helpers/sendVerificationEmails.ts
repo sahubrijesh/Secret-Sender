bb1f9227971fc921681e2d48fdd2f84f3d07fe72
sahubrijesh <thebrijeshsahu@gmail.com>
2024-07-22 19:40:31 +0530

    Add Sign In files

--- src/helpers/sendVerificationEmails.ts
+++ src/helpers/sendVerificationEmails.ts
@@ -1,24 +1,22 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/verificationEmail";
-import { ApiResponse } from "@/types/apiResponse";
+import { ApiResponse } from '@/types/apiResponse';
 
 export async function sendVerificationEmail(
-    username: string, 
-    email: string,
-    verifyCode: string,
+  email: string,
+  username: string,
+  verifyCode: string
 ): Promise<ApiResponse> {
-    try {
-        await resend.emails.send({
-            from: 'onboarding@resend.dev',
-            to: email,
-            subject: 'Secret Sender | Verification Code',
-            react: VerificationEmail({ username, otp: verifyCode }),
-
-        })
-        return {success : true, message : "Verification email sent"}
-    }
-    catch (emailError) {
-        console.error("Error sending email", emailError)
-        return {success : false, message : "Error sending email"}
-    }
+  try {
+    await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: email,
+      subject: 'Mystery Message Verification Code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: 'Verification email sent successfully.' };
+  } catch (emailError) {
+    console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
+  }
 }
